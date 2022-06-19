3ffa048fa53c7a4dd28af1eb63de0d875df8c09c
Aniket <rajeshwarkumar9204@gmail.com>
2022-06-19 14:09:52 +0530

    add useRouter hook in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { useState } from "react";
 
 const Header = () => {
   const [visible, setVisible] = useState("hide");
+  const router = useRouter();
   return (
     <>
       <div className="header">
@@ -19,32 +21,68 @@ const Header = () => {
           <ul className="header__nav">
             <li className="header__nav__item">
               <Link href="/">
-                <a className="header__nav__item__link active">Home</a>
+                <a
+                  className={`${
+                    router.pathname == "/" ? "active" : ""
+                  } header__nav__item__link`}
+                >
+                  Home
+                </a>
               </Link>
             </li>
             <li className="header__nav__item">
               <Link href="/courses">
-                <a className="header__nav__item__link">Courses</a>
+                <a
+                  className={`header__nav__item__link ${
+                    router.pathname == "/courses" ? "active" : ""
+                  }`}
+                >
+                  Courses
+                </a>
               </Link>
             </li>
             <li className="header__nav__item">
               <Link href="/">
-                <a className="header__nav__item__link">Video Gallery</a>
+                <a
+                  className={`header__nav__item__link ${
+                    router.pathname == "/video-gallery" ? "active" : ""
+                  }`}
+                >
+                  Video Gallery
+                </a>
               </Link>
             </li>
             <li className="header__nav__item">
               <Link href="/image-gallery">
-                <a className="header__nav__item__link">Image Gallery</a>
+                <a
+                  className={`header__nav__item__link ${
+                    router.pathname == "/image-gallery" ? "active" : ""
+                  }`}
+                >
+                  Image Gallery
+                </a>
               </Link>
             </li>
             <li className="header__nav__item">
               <Link href="/">
-                <a className="header__nav__item__link">About Pace</a>
+                <a
+                  className={`header__nav__item__link ${
+                    router.pathname == "/about-pace" ? "active" : ""
+                  }`}
+                >
+                  About Pace
+                </a>
               </Link>
             </li>
             <li className="header__nav__item">
               <Link href="/">
-                <a className="header__nav__item__link">Admission Process</a>
+                <a
+                  className={`header__nav__item__link ${
+                    router.pathname == "/admission-process" ? "active" : ""
+                  }`}
+                >
+                  Admission Process
+                </a>
               </Link>
             </li>
           </ul>
