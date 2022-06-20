b4d9bc0915fb0e414fe02c25513617b7bdb74b5c
Aniket <rajeshwarkumar9204@gmail.com>
2022-06-20 16:03:32 +0530

    add video-gallery schema

---
+++ sanity/schemas/videoGallery.js
@@ -0,0 +1,17 @@
+export default {
+  name: "videoGallery",
+  title: "Video Gallery",
+  type: "document",
+  fields: [
+    {
+      name: "videoId",
+      title: "Video Id",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+  ],
+};
