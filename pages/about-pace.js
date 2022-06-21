08c0fa1f5d64b148867d9ff33f24a191dc87c424
Aniket <rajeshwarkumar9204@gmail.com>
2022-06-21 15:27:02 +0530

    add about pace.scss file

--- pages/about-pace.js
+++ pages/about-pace.js
@@ -1,17 +1,34 @@
 import { client } from "../lib/client";
+import PortableText from "@sanity/block-content-to-react";
 
-const AboutPace = ({ sections }) => {
-  console.log(sections);
-  return <div>AboutPace</div>;
+const AboutPace = ({ sections, mentor }) => {
+  console.log(sections, mentor);
+  return (
+    <div>
+      {sections.map((section, index) => {
+        return (
+          <section className="section about-section" key={section._id}>
+            <h1 className="section-title">{section.title}</h1>
+            <PortableText
+              blocks={section.description}
+              className="section-text"
+            />
+          </section>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AboutPace;
 
 export const getServerSideProps = async () => {
   const sections = await client.fetch(`*[_type == "aboutPace"]`);
+  const mentor = await client.fetch(`*[_type == "aboutMentor"]`);
   return {
     props: {
       sections,
+      mentor: mentor[0],
     },
   };
 };
