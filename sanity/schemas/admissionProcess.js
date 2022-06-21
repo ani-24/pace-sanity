c919b15362c617f39a80abe304584e004c22d03b
Aniket <rajeshwarkumar9204@gmail.com>
2022-06-21 16:04:27 +0530

    add admissionProcess schema

---
+++ sanity/schemas/admissionProcess.js
@@ -0,0 +1,18 @@
+export default {
+  name: "admissionProcess",
+  title: "Admission Process",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+};
