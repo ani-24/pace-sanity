aa51cebc369cd376a41230573ad090e14075b1fe
Aniket <rajeshwarkumar9204@gmail.com>
2022-06-21 14:51:30 +0530

    add about pace schema

---
+++ sanity/schemas/aboutPace.js
@@ -0,0 +1,18 @@
+export default {
+  name: "aboutPace",
+  title: "About Pace",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+  ],
+};
