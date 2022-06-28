79eeefbc6bb0231f05b0978a4c7becf58a26beac
Aniket <rajeshwarkumar9204@gmail.com>
2022-06-28 22:22:27 +0530

    add loading progress bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,16 @@ import Head from "next/head";
 import JumpToTop from "./Jump-to-Top";
 import Wame from "./Wame";
 
+import Router from "next/router";
+import nProgress from "nprogress";
+
 const Layout = ({ children }) => {
+  Router.events.on("routeChangeStart", (url) => {
+    nProgress.start();
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    nProgress.done();
+  });
   return (
     <>
       <Head>
@@ -15,6 +24,10 @@ const Layout = ({ children }) => {
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
         />
       </Head>
       <Header />
